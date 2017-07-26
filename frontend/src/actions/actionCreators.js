5cff339b31b327d8e21a2c33f890216dcec899a4
fernandobd42 <fernandolucasbd@gmail.com>
2017-07-26 19:24:33 -0300

    react-feathers-to-do-list-real-time

---
+++ frontend/src/actions/actionCreators.js
@@ -0,0 +1,50 @@
+//SOCKET ACTIONS
+export function createdItem(item) {
+    return {
+        type: 'CREATED_ITEM',
+        item
+    }
+}
+
+export function updatedItem(item) {
+    return {
+        type: 'UPDATED_ITEM',
+        item
+    }
+}
+
+export function removedItem(item) {
+    return {
+        type: 'REMOVED_ITEM',
+        item
+    }
+}
+
+// RESTFUL ACTIONS
+export function createItem(item) {
+    return {
+        type: 'CREATE_ITEM',
+        item
+    }
+}
+
+export function updateItem(id, newData) {
+    return {
+        type: 'UPDATE_ITEM',
+        id,
+        newData
+    }
+}
+
+export function removeItem(id) {
+    return {
+        type: 'REMOVE_ITEM',
+        id
+    }
+}
+
+export function findAllItems() {
+    return {
+        type: 'FIND_ALL_ITEMS'
+    }
+}
